d58b7fcaf8bc637d362d882773fb3b402f2e61a5
MarcusLoboFelipe <78568887+MarcusLoboFelipe@users.noreply.github.com>
2023-06-20 13:41:47 -0300

    Add files via upload

--- tp1.py
+++ tp1.py
@@ -1,59 +1,42 @@
-import socket
 import os
+from flask import Flask, request, send_from_directory
 
-directory = "/scratch/convidado/Área de Trabalho"
+app = Flask(__name__)
 
-port = 8000
+root_dir = '/Users/Marcu/Desktop'
 
-def handle_request(client_socket):
-    request = client_socket.recv(1024).decode()
-    request_parts = request.split(" ")
+@app.route('/')
+def list_files():
+    return generate_directory_listing(root_dir)
 
-    if request_parts[0] == "GET":
-        if request_parts[1] == "/HEADER":
-            header = "HTTP/1.1 200 OK\nContent-type: text/plain\n\n"
-            response = header + str(request)
-            client_socket.sendall(response.encode())
-        else:
-            path = directory + request_parts[1]
-            if os.path.isfile(path):
-                with open(path, "rb") as file:
-                    data = file.read()
-                    header = "HTTP/1.1 200 OK\nContent-type: application/octet-stream\nContent-Disposition: attachment; filename=" + os.path.basename(path) + "\n\n"
-                    response = header.encode() + data
-                    client_socket.sendall(response)
-            else:
-                if os.path.isdir(path):
-                    file_list = os.listdir(path)
-                    file_links = "<br>".join([f"<a href='{os.path.basename(file)}'>{os.path.basename(file)}</a>" for file in file_list])
-                    html = f"<html><body>{file_links}</body></html>"
-                    header = "HTTP/1.1 200 OK\nContent-type: text/html\n\n"
-                    response = header + html
-                    client_socket.sendall(response.encode())
-                else:
-                    response = "HTTP/1.1 404 Not Found\nContent-type: text/plain\n\n404 Not Found"
-                    client_socket.sendall(response.encode())
+@app.route('/<path:path>')
+def download_file(path):
+    file_path = os.path.join(root_dir, path)
+
+    if os.path.isfile(file_path):
+        return send_from_directory(root_dir, path, as_attachment=True)
+    elif os.path.isdir(file_path):
+        return generate_directory_listing(file_path)
     else:
-        response = "HTTP/1.1 400 Bad Request\nContent-type: text/plain\n\n400 Bad Request"
-        client_socket.sendall(response.encode())
+        return "Erro: Página não encontrada", 404
 
-    client_socket.close()
+@app.route('/HEADER')
+def show_header():
+    headers = request.headers  
+    header_list = "<br>".join([f"{header}: {value}" for header, value in headers.items()])
+    return header_list
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def generate_directory_listing(directory):
+    files = os.listdir(directory)
+    file_list = "<ul>"
+    for file in files:
+        file_path = os.path.join(directory, file)
+        file_url = file_path.replace(root_dir, '', 1).lstrip('/')
+        if os.path.isdir(file_path):
+            file += '/'
+        file_list += f"<li><a href='{file_url}'>{file}</a></li>"
+    file_list += "</ul>"
+    return file_list
 
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind(("", port))
-
-server_socket.listen(1)
-print(f"Servidor iniciado na porta {port}")
-
-try:
-    while True:
-        client_socket, client_address = server_socket.accept()
-        handle_request(client_socket)
-except KeyboardInterrupt:
-    pass
-
-server_socket.close()
-
+if __name__ == "__main__":
+    app.run(host='172.18.186.48', port=8000, debug=False)
